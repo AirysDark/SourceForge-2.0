4b369a9be45f556b1756ac1c95b877c48c5726dd
AirysDark <megamanx2sonic@gmail.com>
2025-10-01 12:27:56 -0700

    l

--- overlay/etc/profile.d/10-sf20-main.sh
+++ overlay/etc/profile.d/10-sf20-main.sh
@@ -1,8 +1,4 @@
-# /etc/profile.d/10-sf20-main.sh
-export SF20_NAME="SourceForge 2.0 OS"
-if [ -t 1 ]; then
-  echo ""
-  echo "Welcome to $SF20_NAME"
-  echo "Type 'help' for sfsh commands."
-  echo ""
+#!/bin/sh
+if [ -f /usr/local/sf20/banner.txt ]; then
+  cat /usr/local/sf20/banner.txt
 fi
